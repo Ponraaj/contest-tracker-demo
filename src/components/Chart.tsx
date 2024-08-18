391e6d1e49067b5e3ba2b00925b2a0f494a688d7
Ponraaj <ponraajvetrivel@gmail.com>
2024-08-18 21:19:23 +0530

    "Chart Trial"

---
+++ src/components/Chart.tsx
@@ -0,0 +1,30 @@
+// page.js this is the entry point of application
+
+"use client";
+import React from 'react';
+import dynamic from 'next/dynamic';
+import 'chart.js/auto';
+const Line = dynamic(() => import('react-chartjs-2').then((mod) => mod.Line), {
+  ssr: false,
+});
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May'],
+  datasets: [
+    {
+      label: 'GeeksforGeeks Line Chart',
+      data: [65, 59, 80, 81, 56],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1,
+    },
+  ],
+};
+const LineChart:React.FC = () => {
+  return (
+    <div style={{ width: '700px', height: '700px' }}>
+      <h1>Example 1: Line Chart</h1>
+      <Line data={data} />
+    </div>
+  );
+};
+export default LineChart;
